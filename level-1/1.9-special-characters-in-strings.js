9a622ddc30bc002563b1c6dcc2b25846486dd18c
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-06-07 20:50:02 -0300

    String special characters challenge and answer

---
+++ level-1/1.9-special-characters-in-strings.js
@@ -0,0 +1,8 @@
+/**
+ * Special Characters in Strings
+ * Build the following output in one String, using special characters for the newline, tab stop, and quotation mark:
+ * 
+ * Quoth the raven
+ * 	"Nevermore!"
+ */
+var string = "Quoth the raven:\n\t\"Nevermore!\"";
